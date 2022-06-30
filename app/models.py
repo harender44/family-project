0c0a0d3c61025ccb031188096681b7fe0200eb76
“harender44” <sainiharender44@gmail.com>
2022-06-30 15:59:19 +0530

    initial changes

--- app/models.py
+++ app/models.py
@@ -8,4 +8,31 @@ class Users(db.Model):
     last_name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    
+    date_of_birth = db.Column(db.String(20))
+    gender = db.Column(db.String(10))
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer)
+    gotra = db.Column(db.String(20))
+    religion = db.Column(db.String(20))
+    caste = db.Column(db.String(20))
+    sub_caste = db.Column(db.String(20))
+    mother_tongue = db.Column(db.String(20))
+    native_region = db.Column(db.String(30))
+    native_district = db.Column(db.String(30))
+    place_of_birth = db.Column(db.String(30))
+
+class FamilyDetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer)
+    father_name = db.Column(db.String(30))
+    mother_name = db.Column(db.String(30))
+    grand_father = db.Column(db.String(30))
+    grand_mother = db.Column(db.String(30))
+    great_grand_father = db.Column(db.String(30))
+    great_grand_mother = db.Column(db.String(30))
+    spouse_name = db.Column(db.String(30))
+    kids = db.Column(db.String(100))
+
+
